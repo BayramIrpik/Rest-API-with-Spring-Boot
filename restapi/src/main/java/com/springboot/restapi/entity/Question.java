907013416796f0e163383db627dbff27bc45ae0a
bayram <bayramirpik@gmail.com>
2021-05-18 22:03:00 +0300

    added question entity

---
+++ restapi/src/main/java/com/springboot/restapi/entity/Question.java
@@ -0,0 +1,108 @@
+package com.springboot.restapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "questions")
+public class Question {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="question_text")
+	private String questionText;
+	
+	@Column(name="option_a")
+	private String optionA;
+	
+	@Column(name="option_b")
+	private String optionB;
+	
+	@Column(name="option_c")
+	private String optionC;
+	
+	@Column(name="option_d")
+	private String optionD;
+	
+	@Column(name="correct_option")
+	private String correctOption;
+	
+	public Question() {
+		
+	}
+
+	public Question(long id, String questionText, String optionA, String optionB, String optionC, String optionD,
+			String correctOption) {
+		this.id = id;
+		this.questionText = questionText;
+		this.optionA = optionA;
+		this.optionB = optionB;
+		this.optionC = optionC;
+		this.optionD = optionD;
+		this.correctOption = correctOption;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getQuestionText() {
+		return questionText;
+	}
+
+	public void setQuestionText(String questionText) {
+		this.questionText = questionText;
+	}
+
+	public String getOptionA() {
+		return optionA;
+	}
+
+	public void setOptionA(String optionA) {
+		this.optionA = optionA;
+	}
+
+	public String getOptionB() {
+		return optionB;
+	}
+
+	public void setOptionB(String optionB) {
+		this.optionB = optionB;
+	}
+
+	public String getOptionC() {
+		return optionC;
+	}
+
+	public void setOptionC(String optionC) {
+		this.optionC = optionC;
+	}
+
+	public String getOptionD() {
+		return optionD;
+	}
+
+	public void setOptionD(String optionD) {
+		this.optionD = optionD;
+	}
+
+	public String getCorrectOption() {
+		return correctOption;
+	}
+
+	public void setCorrectOption(String correctOption) {
+		this.correctOption = correctOption;
+	}
+	
+	
+}
